77109540a04fdbf3d99a6c9caeb341ac311e97d5
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-09-26 22:10:54 -0400

    added navbar and layout routing

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,42 @@
+
+<div class="navbar">
+    <img src="/images/sovtlogo_transparent_white.png" class="logo" alt="SO@VT Logo">
+    <div class="button">About Us</div>
+    <div class="button">Contact</div>
+    <div class="button">Hello</div>
+</div>
+
+
+<style>
+    .navbar {
+        position: absolute;
+        width: 100%;
+        top: 0;
+        left: 0;
+        display:flex;
+        align-items: center;
+        justify-content: space-between;
+
+        background-color: black;
+        z-index: 10;
+
+    }
+
+    .button {
+        color: white;
+        height: 100%;
+        width: 10rem;
+        background-color: black;
+        opacity: 0.5;
+        line-height: 45px;
+        text-align: center;
+        font-size: 1rem;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    .logo {
+        width: 5rem;
+    }
+
+</style>
+
