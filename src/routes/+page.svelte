a2d8606110c7bcf2926c94606b8350ef715713d7
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-09-27 22:47:13 -0400

    added two pages for more routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,11 +35,8 @@
 
     .bg-image {
         /* The image used */
-    background-image: url("/images/vtphoto.jpg");
+    background-image: url("/images/vtphoto.png");
     
-    /* Add the blur effect */
-    filter: blur(5px);
-    -webkit-filter: blur(5px);
     
     /* Full height */
     height: 100%; 
@@ -55,57 +52,77 @@
     }
 
     body {
-    margin: 0;
-    height: 100vh;
-    /* display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: white; */
-    overflow: hidden;
-}
+        margin: 0;
+        height: 100vh;
+        /* display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: white; */
+        overflow: hidden;
+    }
 
 
-.content {
+    .content {
 
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 50%;
-    left: 45%;
-    transform: translate(-50%, -50%);
-    width: 90%;   
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        top: 50%;
+        left: 45%;
+        transform: translate(-50%, -50%);
+        width: 90%;   
 
-}
+    }
 
-.logo {
+    .logo {
 
-    width: 30rem;
-    padding: 5rem;
-}
+        width: 30rem;
+        padding: 5rem;
+    }
 
-.border {
-    width: 1rem;
-   
-    height: 30rem;
+    .border {
+        width: 1rem;
+    
+        height: 30rem;
 
-}
+    }
 
-.border.b {
-    border-right: 5px solid orange;
-}
+    .border.b {
+        border-right: 5px solid orange;
+        animation: growHeight ease 2s 1 forwards;
+    }
 
-.invy-banner {
+    .invy-banner {
 
-    text-align: center;
-    font-size: 2rem;
-    font-family: 'Montserrat', sans-serif;
-    color: white;
+        text-align: center;
+        font-size: 2rem;
+        font-family: 'Montserrat', sans-serif;
+        color: white;
 
-    padding: 7rem;
+        padding: 7rem;
+        animation: fadeInAnimation ease 2s 1 forwards;
 
-}
+    }
+
+    @keyframes growHeight {
+        0% {
+            height: 0;
+        }
+        100% {
+            height: 30rem;
+        }
+    }
+
+    @keyframes fadeInAnimation {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
 
 
 </style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,56 @@
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</svelte:head>
+
+
+<script>
+    import Bio from '$lib/Bio.svelte';
+
+    const group_data = [["Executive Board", 6], ["Tournament Logistics", 5], ["Volunteering", 3], ["Outreach", 4], ["Public Relations", 2]];
+
+</script>
+
+
+{#each group_data as group}
+
+    <div class="group">
+        <div class="title">
+            {group[0]}
+        </div>
+
+        <div class="bios">
+            {#each Array(group[1]) as _}
+                <Bio />
+
+            {/each}
+        </div>
+
+
+    </div>
+
+{/each}
+
+
+
+<style>
+
+    .group .title{
+
+        font-family: 'Montserrat', sans-serif;
+        font-size: 2rem;
+        text-align: center;
+        margin-top: 10rem;
+
+    }
+
+    .bios {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    body {
+        overflow: scroll;
+    }
+
+</style>
