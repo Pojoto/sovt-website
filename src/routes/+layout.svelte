c17f414b640814009e23c093fe69bee40c1b7ec2
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-09-27 20:43:17 -0400

    added layout template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,10 @@
 
+<svelte:head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0">
+</svelte:head>
+
+
+
 <script>
     import Navbar from '$lib/Navbar.svelte';
 </script>
