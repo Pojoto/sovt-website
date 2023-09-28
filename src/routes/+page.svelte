50b9602480edf938be43bb0f6a2e3373f47ed649
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-09-27 20:43:34 -0400

    added page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,111 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+    import Button from '$lib/Button.svelte';
+</script>
+
+<body>
+    <div class='bg-image'></div>
+
+    <div class="content">
+
+        <img src="/images/sovtlogo_transparent_white.png" class="logo" alt="Main SO@VT Logo">
+
+        <div class="border b"></div>
+        <div class="border"></div>
+
+        <div class="invy-banner">
+            Division C. Invitational
+            <br>
+            <b>January 27, 2024</b>
+            <br>
+            <br>
+            <Button text="More Info" url="/tournament-info"/>
+        </div> 
+
+    </div>
+
+</body>
+
+
+<style>
+
+    .bg-image {
+        /* The image used */
+    background-image: url("/images/vtphoto.jpg");
+    
+    /* Add the blur effect */
+    filter: blur(5px);
+    -webkit-filter: blur(5px);
+    
+    /* Full height */
+    height: 100%; 
+    width: 100%;
+    
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* background-repeat: no-repeat;
+    background-attachment: fixed;  
+    background-size: cover; */
+    }
+
+    body {
+    margin: 0;
+    height: 100vh;
+    /* display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white; */
+    overflow: hidden;
+}
+
+
+.content {
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    transform: translate(-50%, -50%);
+    width: 90%;   
+
+}
+
+.logo {
+
+    width: 30rem;
+    padding: 5rem;
+}
+
+.border {
+    width: 1rem;
+   
+    height: 30rem;
+
+}
+
+.border.b {
+    border-right: 5px solid orange;
+}
+
+.invy-banner {
+
+    text-align: center;
+    font-size: 2rem;
+    font-family: 'Montserrat', sans-serif;
+    color: white;
+
+    padding: 7rem;
+
+}
+
+
+</style>
---
+++ src/routes/tournament-info/+page.svelte
@@ -0,0 +1,49 @@
+
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+    import Card from '$lib/Card.svelte'; 
+</script>
+
+
+<body>
+    <div class="title">
+        2024 Virginia Tech Invitational
+    </div>
+
+    <div class="cards">
+        <Card title="Date" content="1/27/24"/>
+        <Card title="Division" content="C"/>
+        <Card title="Team # Cap" content="32"/>
+        <Card title="Cost" content="$100*"/>
+    </div>
+
+    The Virginia Tech Invitational will be held in-person on Virginia Tech's campus in Blacksburg, Virginia.
+
+</body>
+
+
+
+
+
+<style>
+
+    body {
+        padding-top: 10rem;
+    }
+
+    .title {
+        text-align: center;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 3rem;
+    }
+
+    .cards {
+        display: flex;
+        align-items: center;
+        margin-top: 2rem;
+    }
+
+</style>
