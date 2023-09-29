78aa2216980a6c2a8b89525c31f6110b3e4b9b26
Pojoto <114115897+Pojoto@users.noreply.github.com>
2023-09-29 13:51:20 -0400

    added hamburger menu for mobile

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,15 +1,40 @@
 
+<script>
+
+    let on = false;
+
+
+</script>
+
 <div class="navbar">
-    <div class=""><a href="/"><img src="/images/navbar_logo.png" class="logo" alt="SO@VT Logo"><a/></div>
+    <div class=""><a href="/" on:click={() => {on = false;}}><img src="/images/navbar_logo.png" class="logo" alt="SO@VT Logo"><a/></div>
     <div class="buttons">
         <a href="/tournament-info" class="button">Tournament Info</a>
         <!-- <a href="/about" class="button">About Us</a> -->
         <a href="/our-team" class="button">Our Team</a>
         <a href="/volunteer" class="button">Volunteer</a>
         <a href="/contact" class="button">Contact</a>
+        
+    </div>
+
+    <a href="javascript:void(0);" on:click={() => {on = !on}} id="hamburger"><img src="images/hamburger.png" class="hamburger" alt=""Hamburger Menu></a>
+
+</div>
+
+<div class="menu" style={on ? "display:block":"display:none"}>
+
+    <div class="buttons2">
+        <a href="/tournament-info" on:click={() => {on = false}} class="button">Tournament Info</a>
+        <!-- <a href="/about" class="button">About Us</a> -->
+        <a href="/our-team" on:click={() => {on = false}} class="button">Our Team</a>
+        <a href="/volunteer" on:click={() => {on = false}} class="button">Volunteer</a>
+        <a href="/contact" on:click={() => {on = false}} class="button">Contact</a>
+        
     </div>
 
 </div>
+
+
 
 
 <style>
@@ -46,12 +71,30 @@
         transition: 0.3s;
     }
 
+    .hamburger {
+        width: 0rem;
+        height: 3rem;
+        display: block;
+    }
+
+    .menu {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        background-color: rgba(0,0,0,1);
+        z-index: 1;
+        text-align: center;
+
+        height: 20rem;
+    }
+
     .button:hover {
         color: orange;
 
     }
 
-    a {
+    .button {
         text-decoration: none;
         display: block;
         margin:auto;
@@ -65,6 +108,40 @@
     .buttons {
         display: flex;
         align-items: center;
+    }
+
+    .buttons2 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        margin-top: 5rem;
+        left: 50%;
+        transform: translate(-50%, 0);
+
+    }
+
+    @media (max-width: 1000px) {
+        .buttons {
+            display: none;
+        }   
+
+        .button {
+            height: 1rem;
+            width: 100vw;
+            line-height: 1rem;
+        }
+
+        .hamburger {
+            width: 3rem;
+            margin-right: 2rem;
+        }
+
+        .menu {
+            display: none;
+        }
+        
     }
 
 </style>
