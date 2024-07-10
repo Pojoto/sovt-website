5b09236daf1ef388ba69a28662b4a100f9d2d242
Joshua Gao <114115897+Pojoto@users.noreply.github.com>
2024-07-10 16:18:59 -0500

    Updated date for 2025 invy

--- src/routes/tournament-info/+page.svelte
+++ src/routes/tournament-info/+page.svelte
@@ -11,33 +11,21 @@
     <div class="title">
         <b>2025 Virginia Tech Invitational</b>
     </div>
-    
-    <div class="info">    
-    
-        <div class="section">
-            <h2>
-                Registration
-            </h2>
-        
-            <p style="">
-                Registration details for the 2025 Invitational are <b>TBD</b>.
-                <br><br>
-            </p>
-    
-            <p style="">
-                <!-- We welcome both veteran and new teams to gain competition experience at our tournament. If you are a Virginia school and qualify for the VASO Outreach Initiative, we will waive half of your registration fees. 
-                If your school otherwise has special circumstances that prevent you from paying the registration fees, 
-                please reach out to us at scienceolympiadvt@gmail.com
-        
-                <br><br> -->
-                <Button text="Register Here" url="https://docs.google.com/forms/d/e/1FAIpQLScBBQE8dZRdnpWDDE7f8RkJL2gYuzKzbp78SMWQRXhukXOaqQ/viewform?usp=sf_link" active={false}/>
-        
-                <b style="font-size:.75rem">Registration opens TBD</b>
 
-                <br><br><br><br>
-        
-            </p>
-        </div>
+    <div class="division">Division C</div>
+    <br>
+    <div class="division">February 1, 2025</div>
+    
+    <div class="cards">
+        <Card title="Date" content="2/1/25"/>
+        <Card title="Division" content="C"/>
+        <Card title="Team # Cap" content="TBD"/>
+        <Card title="Cost" content="TBD"/>
+    </div>
+    
+    <div class="info">
+        <b>The date for the 2025 Virginia Tech Invitational has been decided! More details are to come.</b>
+    
 
         <div class="section">
 
@@ -47,39 +35,49 @@
             <p style='text-align:left'>We will have opportunities for exclusive university lab tours, 
                 connecting with VT students and programs, and meeting the Hokie 
                 Bird (our wonderful school mascot!). We encourage students to 
-                explore VT’s beautiful campus and award-winning dining halls during our 
+                explore VT’s campus and award-winning dining halls during our 
                 tournament.</p>
 
                 <br>
 
             <h1 class="subtitle">Build Events</h1>
-            <p style='text-align:left'>We work closely with VASO to align 
-                build testing with Regionals and State tournament standards. These standards 
-                normally resemble National rules, so out-of-state teams are still encouraged to come! 
-                </p>
+            <p style='text-align:left'>We’re working closely with VASO to align 
+                build testing with Regionals and State tournament standards. 
+                We expect to run all builds this year.</p>
 
                 <br>
 
             <h1 class="subtitle">Test Events</h1>
             <p style='text-align:left'>Our writers are experienced Science Olympiad 
                 alumni from Virginia Tech and other colleges. 
-                Tests go through rounds of thorough review and revision to ensure quality. 
-                As seasoned SO alumni, we know what high quality tests look like and how to achieve them.</p>
+                Tests go through rounds of thorough review and revision to ensure quality.</p>
 
                 <br>
 
             <h1 class="subtitle">Early-Spring Regionals Prep</h1>
             <p style='text-align:left'>We are targeting our invitational to give teams the
-                 best competitive start to the Regionals/States season. We aim for an early-year competition date.</p>
+                 best competitive start to the Regionals/States season. 
+                 Virginia teams can return for our Virginia Tech Regional 
+                 on February 17th.</p>
 
+            
                  <br>
 
             <h1 class="subtitle">Webinars and Support</h1>
             <p style='text-align:left'>We host an annual Q&A webinar leading up 
                 to our competition, where SO alumni give competition advice. 
                 We’d love to answer any questions you have about our tournament 
-                or Science Olympiad in general.</p>
+                or team logistics in general.</p>
 
+            <!-- <h2>
+                Tests
+            </h2>
+        
+            <p>
+                Tournament quality is extremely important to us. Tests will be written by experienced Science Olympiad alumni from Virginia Tech and around the country. 
+                Each test goes through a series of checks and deadlines to ensure the highest quality. <br>
+                <br>
+            </p> -->
         </div>
 
         <div class="section">
@@ -92,6 +90,7 @@
         
             </p>
         </div>
+
 
     </div>
 </div>
